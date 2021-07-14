36f25f09e2745c984e0bfdcc22b213179e80f18f
bencode <bencode@163.com>
2021-07-15 00:50:40 +0800

    finish C1W3

--- C1/W2/assignment/C1_W2_Assignment.js
+++ C1/W2/assignment/C1_W2_Assignment.js
@@ -1,7 +1,7 @@
 import { FMnistData } from './fashion-data.js';
 
 let canvas, ctx, saveButton, clearButton;
-let pos = { x:0, y:0 };
+let pos = { x: 0, y: 0 };
 let rawImage;
 let model;
 
@@ -9,12 +9,12 @@ function getModel() {
     model = tf.sequential();
 
     model.add(tf.layers.conv2d({ inputShape: [28, 28, 1], kernelSize: 3, filters: 8, activation: 'relu' }));
-    model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
-    model.add(tf.layers.conv2d({filters: 16, kernelSize: 3, activation: 'relu'}));
-    model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+    model.add(tf.layers.conv2d({ filters: 16, kernelSize: 3, activation: 'relu' }));
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
     model.add(tf.layers.flatten());
-    model.add(tf.layers.dense({units: 128, activation: 'relu'}));
-    model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+    model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
     
 	model.compile({optimizer: tf.train.adam(), loss: 'categoricalCrossentropy', metrics: ['accuracy']});
     
@@ -89,8 +89,6 @@ function save() {
     const classNames = ["T-shirt/top", "Trouser", "Pullover", 
                       "Dress", "Coat", "Sandal", "Shirt",
                       "Sneaker",  "Bag", "Ankle boot"];
-            
-            
     alert(classNames[pIndex]);
 }
     
--- C1/W2/ungraded_lab/script.js
+++ C1/W2/ungraded_lab/script.js
@@ -1,19 +1,19 @@
-import {MnistData} from './data.js';
-var canvas, ctx, saveButton, clearButton;
-var pos = {x:0, y:0};
-var rawImage;
-var model;
+import { MnistData } from './data.js';
+let canvas, ctx, saveButton, clearButton;
+let pos = { x: 0, y: 0 };
+let rawImage;
+let model;
 	
 function getModel() {
 	model = tf.sequential();
 
-	model.add(tf.layers.conv2d({inputShape: [28, 28, 1], kernelSize: 3, filters: 8, activation: 'relu'}));
-	model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
-	model.add(tf.layers.conv2d({filters: 16, kernelSize: 3, activation: 'relu'}));
-	model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
+	model.add(tf.layers.conv2d({ inputShape: [28, 28, 1], kernelSize: 3, filters: 8, activation: 'relu' }));
+	model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+	model.add(tf.layers.conv2d({ filters: 16, kernelSize: 3, activation: 'relu' }));
+	model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
 	model.add(tf.layers.flatten());
-	model.add(tf.layers.dense({units: 128, activation: 'relu'}));
-	model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+	model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+	model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
 
 	model.compile({optimizer: tf.train.adam(), loss: 'categoricalCrossentropy', metrics: ['accuracy']});
 
